3710c1ecedbeabc8653c2ac335aaf71cb41af083
Jan-Gerd Tenberge <janten@gmail.com>
2017-03-14 15:34:59 +0100

    Initial Commit

---
+++ window-key/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  window-key
+//
+//  Created by Jan-Gerd Tenberge on 14.03.17.
+//  Copyright © 2017 Jan-Gerd Tenberge. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+
+	// Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+	[super setRepresentedObject:representedObject];
+
+	// Update the view, if already loaded.
+}
+
+
+@end
---
+++ window-key/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  window-key
+//
+//  Created by Jan-Gerd Tenberge on 14.03.17.
+//  Copyright © 2017 Jan-Gerd Tenberge. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
